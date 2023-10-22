3d9742ca7aec1f4d2382c0a73c627467e6e48e2a
Osama-creatore <osskh1221@gmail.com>
2023-10-22 21:26:41 +0300

    moveZeroes

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,42 +1,14 @@
 void main(List<String> arguments) {
-  bool areAllEqual(List<dynamic> list) {
-    if (list.isEmpty) {
-      return true;
+  void moveZeroes(List<int> nums) {
+    int listLenght = nums.length;
+    nums.removeWhere((element) => element == 0);
+    int listRemaningLenght = listLenght - nums.length;
+    for (var i = 0; i < listRemaningLenght; i++) {
+      nums.add(0);
     }
-
-    final firstItem = list[0];
-    for (var item in list) {
-      if (item != firstItem) {
-        return false;
-      }
-    }
-    return true;
   }
 
-  String longestCommonPrefix(List<String> strs) {
-    List checkList = [];
-    List<String> myReturnList = [];
-    String commenPrefix = "";
-    for (var c = 0; c < 100; c++) {
-      for (var s = 0; s < strs.length; s++) {
-        if (s >= 0 && s < strs.length && c >= 0 && c < strs[s].length) {
-          checkList.add(strs[s][c].toLowerCase());
-        } else {
-          break;
-        }
-      }
-      if (areAllEqual(checkList) && checkList.length == strs.length) {
-        checkList.isNotEmpty
-            ? myReturnList.add(checkList[0])
-            : checkList.clear();
-      } else {
-        break;
-      }
-      checkList.clear();
-      commenPrefix = myReturnList.join();
-    }
-    return commenPrefix;
-  }
-
-  print(longestCommonPrefix(["ab", "a"]));
+  moveZeroes([0, 1, 0, 3, 12]);
 }
+
+// [[s,o,o],[o, ,o]]
