f3800d5af0b1ca627b05691b5b84ccd3c675cf40
Osama-creatore <osskh1221@gmail.com>
2023-10-24 17:22:46 +0300

    isPalindrome

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,14 +1,30 @@
 void main(List<String> arguments) {
-  void moveZeroes(List<int> nums) {
-    int listLenght = nums.length;
-    nums.removeWhere((element) => element == 0);
-    int listRemaningLenght = listLenght - nums.length;
-    for (var i = 0; i < listRemaningLenght; i++) {
-      nums.add(0);
+  int findCenterIndex(List numbers) {
+    int length = numbers.length;
+    if (length % 2 == 1) {
+      int centerIndex = length ~/ 2;
+      return centerIndex;
+    } else {
+      return -1;
     }
   }
 
-  moveZeroes([0, 1, 0, 3, 12]);
-}
+  bool isPalindrome(int x) {
+    List mainList = x.toString().split("");
+    int listCenter = findCenterIndex(mainList);
 
-// [[s,o,o],[o, ,o]]
+    if (listCenter != -1) {
+      mainList.removeAt(listCenter);
+    }
+    final middle = mainList.length ~/ 2;
+    List part1 = mainList.sublist(0, middle);
+    List part2 = mainList.sublist(middle);
+    if (part1.join() == part2.reversed.join()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  print(isPalindrome(11));
+}
